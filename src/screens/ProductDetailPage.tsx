cc60e406200649fa0cad0753726c2e60ff825cf6
Baxtiyor <baxtiyorshukurjanov36@gmail.com>
2023-03-07 21:28:09 +0200

    create new component page  productDetailPage

--- src/screens/ProductDetailPage.tsx
+++ src/screens/ProductDetailPage.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, FlatList, Image } from 'react-native'
+import { View, Text, StyleSheet, FlatList, Image, ScrollView } from 'react-native'
 import React from 'react'
 import products from '../data/products'
 import { useWindowDimensions } from 'react-native'
@@ -8,7 +8,9 @@ const product = products[0];
 const {width} = useWindowDimensions();
 
   return (
-    <View style={styles.container}>
+
+    <View>
+        <ScrollView>
        <FlatList
          data={product.images}
          renderItem={({item}) => (
@@ -17,14 +19,42 @@ const {width} = useWindowDimensions();
          horizontal
          showsHorizontalScrollIndicator={false}
          pagingEnabled
-
        />
-    </View>
+
+      <View style={{padding:20}}>
+       <Text style={styles.name}>{product.name}</Text>
+       <Text style={styles.price}>${product.price}</Text>
+       <Text style={styles.description}>{product.description}</Text>
+       </View>
+       </ScrollView>
+     </View>
+
   )
 }
 
 const styles = StyleSheet.create({
+   name:{
+      marginHorizontal:10,
+      color: "gray",
+      fontSize: 30,
+      letterSpacing:0.2,
+      fontWeight:"bold"
 
+   },
+   price:{
+    marginHorizontal:10,
+    color: "black",
+    fontWeight:"bold",
+    fontSize: 20,
+
+
+   },
+   description:{
+    color: "gray",
+    marginHorizontal:10,
+    fontSize: 15,
+    lineHeight: 23
+   }
 
   });
 
